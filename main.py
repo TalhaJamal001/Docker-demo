6ace816c0420e1f61e345e36e5101c077315ba58
TalhaJamal001 <74287804+TalhaJamal001@users.noreply.github.com>
2024-08-26 18:09:30 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,4 +5,4 @@ def run():
     return "{\"message\":\"Hey there pythin\")"
 
 if __name__ == "__main__":
-    helloworld.run(host="0.0.0.0", port=int("3000"), debug=True
+    helloworld.run(host="0.0.0.0", port=int("3000"), debug=True)
